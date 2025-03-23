47a2a49b942353d06bf73a80f8934752e030c198
william <mbonguefeukou@gmail.com>
2025-03-23 20:38:41 +0100

    Mise à jour du projet

--- config/config.php
+++ config/config.php
@@ -1,8 +1,3 @@
 <?php
-    try {
-        $pdo = new PDO("mysql:host=localhost;dbname=projet_brief","root","",
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
-    } catch (PDOException $e) {
-        echo" Une erreur". $e->getMessage();
-    }
+    
 ?>
--- models/mod_ins.php
+++ models/mod_ins.php
@@ -1,34 +1,3 @@
 <?php
-    require '../config/config.php';
-
-    if (isset($_POST["submit"])) {
-        $username = trim($_POST["nom"]);
-        $email = trim($_POST["email"]);
-        $selec = trim($_POST["choix"]);
-        $pass = trim($_POST["password"]);
-        if(empty($nom) || empty($email) || empty($selec) || empty($pass)){
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $stmt = $pdo->prepare("SELECT id FROM users WHERE email = ?");
-                $stmt->execute([$email]);
-            
-
-                if ($stmt->fetch()) {
-                    echo "<script>alert('Cet email est déjà utilisé.'); window.history.back();</script>";
-                    exit;
-                }
-
-                $hashed_password = password_hash($pass, PASSWORD_DEFAULT);
-                $stmt = $pdo->prepare("INSERT INTO users (username, email, password, role_id) VALUES (?, ?, ?, ?)");
-
-                if ($stmt->execute([$username, $email, $hashed_password, $selec])) {
-                    echo "<script>alert('Inscription réussie !'); window.location.href = '../views/liste_session_admin.php';</script>";
-                } else {
-                    echo "<script>alert('Erreur lors de l'inscription.'); window.history.back();</script>";
-                }
-            }
-            else {
-                echo "<script>alert('Format d'email invalide.'); window.history.back();</script>";
-                exit;
-            }
-        }
-    }
+    
+?>
